cae5d23a9fc813da14bab8d533e977f9a8e00b01
DoganM95 <doganmermertas@hotmail.de>
2022-02-18 20:54:45 +0100

    quick reminder

--- Server/ESP32/Software/Configuration/Blynk_example.h
+++ Server/ESP32/Software/Configuration/Blynk_example.h
@@ -3,4 +3,4 @@ const bool BLYNK_USE_LOCAL_SERVER = false;
 
 // Following settings are obsolete, if BLYNK_USE_LOCAL_SERVER is set to false
 const char* BLYNK_SERVER = "yourBlynkServer.ddns.net";
-const unsigned short int BLYNK_PORT = 8080;
+const unsigned short int BLYNK_PORT = 8080;  // http port of server, not https
